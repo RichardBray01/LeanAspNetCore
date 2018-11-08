9e90e5fb75d39526b8d52b195edf0ab30cefc8ab
Richard Bray <rjbray1@gmail.com>
2018-11-08 14:09:10 +0000

    Add project files.

---
+++ clientapp/webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+module.exports = (env = {}, argv = {}) => {
+    const config = {
+        mode: argv.mode || 'development', // we default to development when no 'mode' arg is passed
+        entry: {
+            main: './js/main.js'
+        },
+        output: {
+            filename: '[name].js',
+            path: path.resolve(__dirname, '../wwwroot/dist'),
+            publicPath: "/dist/"
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.css$/,
+                    use: [
+                        'style-loader',
+                        'css-loader'
+                    ]
+                },
+                {
+                    test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                    use: [
+                        {
+                            loader: 'file-loader',
+                            options:
+                            {
+                                name: '[name].[ext]',
+                                outputPath: 'fonts/'
+                            }
+                        }
+                    ]
+                }
+            ]
+        }
+    }
+
+    return config;
+};
